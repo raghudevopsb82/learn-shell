3f0c0d3e9d4b1dd0883150be94f13e7b73079dcc
Raghu K <raghuk.vit@gmail.com>
2024-12-11 06:47:29 +0530

    RoboSHop Project

---
+++ 03-functions.sh
@@ -0,0 +1,17 @@
+# If we give a name to a set of commands then it is called as function.
+
+# Syntax: Declaring a function
+# function_name() {
+#   commands
+# }
+
+# Call a function
+# sample
+
+
+sample() {
+  ls -l
+  pwd
+}
+
+sample
