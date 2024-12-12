671aad1e2b96bbfaad34dd77c15385d1703a5b43
Raghu K <raghuk.vit@gmail.com>
2024-12-12 06:35:58 +0530

    RoboSHop Project

---
+++ 06-echo.sh
@@ -0,0 +1,16 @@
+echo Hello
+
+# Color         FG        BG
+# Red           31        41
+# Green         32        42
+# Yellow        33        43
+# Blue          34        44
+# Magenta       35        45
+# Cyan          36        46
+
+# Syntax: echo -e "\e[COLmMESS\e[0m"
+echo -e "\e[31mHello World\e[0m"
+
+
+
+
